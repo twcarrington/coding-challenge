55523b526dd3f2a89864e110a069b5da305448e4
Tommy <twcarrington@gmail.com>
2017-05-05 12:09:19 -0400

    finally got firebase CLI package configured.  Local firebase server and deployed app actually render...but database calls are failing

--- chat-challenge/src/App.js
+++ chat-challenge/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
+import {config} from "./FirebaseConfig.js";
 
 class App extends Component {
 
@@ -17,17 +19,33 @@ class App extends Component {
     this.setState({currentMessage: event.target.value});
   }
 
+  handleSubmit(event) {
+    firebase.database().ref("/user1").set({
+      message: this.state.currentMessage
+    });
+  }
+
+  componentDidMount(
+    firebase.database().ref("/").set({
+      message: "blahblahblah"
+    });
+  )
 
   render() {
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <div className ="input-field">
-          <input type ="text" className ="validate" value ={this.state.currentMessage} onChange ={this.changeCurrentMessage}></input>
-          <label data-error ="you must enter text"> Current Message </label>
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="input-field">
+            <input type="text" className="validate" value={this.state.currentMessage} onChange={this.changeCurrentMessage}></input>
+            <label data-error="you must enter text"></label>
+          </div>
 
+          <br></br>
+
+          <button type="submit" name="action"> send current message </button>
+        </form>
       </div>
     );
   }
